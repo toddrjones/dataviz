dd38fecf8ee56adeefaad66936a696ed85ec6a3f
toddrjones <52247393+toddrjones@users.noreply.github.com>
2020-03-07 19:20:23 -0500

    Update cmort_github_v1.R

--- childmortality/cmort_github_v1.R
+++ childmortality/cmort_github_v1.R
@@ -6,12 +6,12 @@ library(RColorBrewer)
 #set your working directory here
 setwd("")
 
-#download from https://www.gapminder.org/data/, and save to /data folder
+#download relevant files from https://www.gapminder.org/data/, and save to /data folder
 gdp_w <- read.csv("data/income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
 mort_w <- read.csv("data/child_mortality_0_5_year_olds_dying_per_1000_born.csv")
 pop_w <- read.csv("data/population_total.csv")
 
-#make data from wide to long
+#get data from wide to long
 gdp <- gather(gdp_w, Year, gdp, starts_with("X"))
 mort <- gather(mort_w, Year, mort, starts_with("X"))
 pop <- gather(pop_w, Year, pop, starts_with("X"))
@@ -35,11 +35,9 @@ data$lgdp <- log(data$gdp)
 data$year <- as.integer(data$year)
 
 #merge on continent - not used in current plot, but can be useful
-data$continent <- countrycode(sourcevar = data[, "country"],
-							  origin = "country.name",
-							  destination = "continent")
+data$continent <- countrycode(sourcevar = data[, "country"], origin = "country.name", destination = "continent")
 
-#set the color scale here
+#set the color scale 
 colourCount = length(unique(data$mort))
 getPalette = colorRampPalette(brewer.pal(9, "Blues"))
 
@@ -52,14 +50,14 @@ map <-   ggplot(data, aes(x = lgdp, y = mort)) +
 	theme(plot.caption = element_text(size=16)) +
 	theme(plot.title = element_text(hjust = 0.5)) +
 	theme(plot.subtitle = element_text(size=16, hjust = 0.5, vjust=-10)) +
-	theme(axis.text=element_text(size=16),
-		  axis.title=element_text(size=16)) +	
+	theme(axis.text=element_text(size=16), axis.title=element_text(size=16)) +	
 	labs(x = "Log GDP Per Capita, PPP",
 		 y = "0-5 Year Old Deaths/1000 Born") +
 	labs(caption = "By @toddrjones. Source: Gapminder. ") +
 	transition_time(year) +	
 	labs(subtitle = paste('Year: {frame_time}'))
 
+#save in /output folder
 anim_save("output/cmort.gif", map, end_pause=12, width = 600, height = 550, duration=22, nframes=220)
 
 
